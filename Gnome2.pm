fed3444f2841787117f4bc34f06ae9325f185b0d
Torsten Schönfeld <tsch@src.gnome.org>
2003-10-04 14:35:11 +0000

    Alter documentation to better describe Gnome2. Don't repeat what Gtk2.pm
    	* Gnome2.pm: Alter documentation to better describe Gnome2.  Don't
    	repeat what Gtk2.pm says, but rather point the reader to it.  Add
    	links to the API documentation.  Add a MISSING METHODS section, a
    	RENAMED METHODS section and a DIFFERENT CALL SIGNATURES OR
    	SEMANTICS section.  (Following Gtk2.pm's example.)
    
    svn path=/trunk/; revision=126

--- Gnome2.pm
+++ Gnome2.pm
@@ -40,52 +40,134 @@ Gnome2 - Perl interface to the 2.x series of the Gnome libraries
 
 =head1 ABSTRACT
 
-  Perl bindings to the 2.x series of the Gtk+ widget set.
-  This module allows you to write graphical user interfaces in a perlish
-  and object-oriented way, freeing you from the casting and memory 
-  management in C, yet remaining very close in spirit to original API.
+Perl bindings to the 2.x series of the Gnome libraries.  This module allows you
+to interface with the libgnome and libgnomeui library to create Gnome
+applications.
 
 =head1 DESCRIPTION
 
-The Gnome2 module allows a perl developer to use the Gnome libraries.
-Find out more about Gnome+ at http://www.gnome.org.
+Since this module tries to stick very closely to the C API.  The documentation
+found at
 
-To discuss gtk2-perl, ask questions and flame/praise the authors,
-join gtk-perl-list@gnome.org at lists.gnome.org.
+  http://developer.gnome.org/doc/API/2.0/libgnome/index.html
 
-If you have a web site set up for your module, mention it here.
+and
 
-FIXME we have no other documentation, but we probably need it.
+  http://developer.gnome.org/doc/API/2.0/libgnomeui/index.html
+
+is the canonical reference.
+
+The mapping described in L<Gtk2::api>(3pm) also applies to this module.
+
+To discuss this module, ask questions and flame/praise the authors, join
+gtk-perl-list@gnome.org at lists.gnome.org.
+
+=head1 MISSING METHODS
+
+=over
+
+=item gnome_app_fill_menu_with_data
+
+=item gnome_app_fill_menu_custom
+
+Use gnome_app_fill_menu instead.
+
+=item gnome_app_fill_toolbar_with_data
+
+=item gnome_app_fill_toolbar_custom
+
+Use gnome_app_fill_toolbar instead.
+
+=item gnome_app_create_menus_interp
+
+=item gnome_app_create_menus_with_data
+
+=item gnome_app_create_menus_custom
+
+Use gnome_app_create_menus instead.
+
+=item gnome_app_create_toolbar_interp
+
+=item gnome_app_create_toolbar_with_data
+
+=item gnome_app_create_toolbar_custom
+
+Use gnome_app_create_toolbar instead.
+
+=item gnome_app_insert_menus_custom
+
+=item gnome_app_insert_menus_with_data
+
+=item gnome_app_insert_menus_interp
+
+Use gnome_app_insert_menus instead.
+
+=back
+
+=head1 RENAMED METHODS
+
+=over
+
+=item gnome_program_get
+
+Gnome2::Program->get has been renamed to ->get_program to avoid a clash with
+GObject->get.
+
+=back
+
+=head1 DIFFERENT CALL SIGNATURES OR SEMANTICS
+
+=over
+
+TODO: GnomeUIInfo.
+
+=item gnome_client_set_restart_command
+
+=item gnome_client_set_discard_command
+
+=item gnome_client_set_resign_command
+
+=item gnome_client_set_shutdown_command
+
+=item gnome_client_set_clone_command
+
+Instead of argc and argv you simply pass in a list.
+
+=item gnome_program_init
+
+Gnome2::Program->init automagically fills argc and argv so just omit these
+parameters.
+
+=over
 
 =head1 SEE ALSO
 
-L<perl>(1), L<Glib>(3pm), L<Gtk2>(3pm).
+L<perl>(1), L<Glib>(3pm), L<Gtk2>(3pm), L<Gtk2::api>(3pm).
 
 =head1 AUTHOR
 
 muppet E<lt>scott at asofyet dot orgE<gt> did the initial stuff.
 
-Torsten Schoenfeld E<lt>kaffeetisch at web dot deE<gt> took over between
-0.34 and 0.36, and implemented lots of tests and missing functionality,
-and corrected several of muppet's mistakes.
+Torsten Schoenfeld E<lt>kaffeetisch at web dot deE<gt> took over between 0.34
+and 0.36, and implemented lots of tests and missing functionality, and
+corrected several of muppet's mistakes.
 
 =head1 COPYRIGHT AND LICENSE
 
 Copyright 2003 by the gtk2-perl team.
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Library General Public
-License as published by the Free Software Foundation; either
-version 2 of the License, or (at your option) any later version.
+This library is free software; you can redistribute it and/or modify it under
+the terms of the GNU Library General Public License as published by the Free
+Software Foundation; either version 2 of the License, or (at your option) any
+later version.
 
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Library General Public License for more details.
+This library is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Library General Public License for more
+details.
 
-You should have received a copy of the GNU Library General Public
-License along with this library; if not, write to the 
-Free Software Foundation, Inc., 59 Temple Place - Suite 330, 
-Boston, MA  02111-1307  USA.
+You should have received a copy of the GNU Library General Public License along
+with this library; if not, write to the Free Software Foundation, Inc., 59
+Temple Place - Suite 330, Boston, MA 02111-1307 USA.
 
 =cut
